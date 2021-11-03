1f2f6769d8b0cf7da91f3741612bcd9d47596e53
Ting Wei Zhuang <ji394sfx87@gmail.com>
2021-11-03 14:14:29 +0800

    feat: 增加播放器外框架構
    1. 新增 example.mp4 範例影片
    2. 新增 VideoPlayer 及 VideoContainer 播放器外框

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,44 @@
+import styled from "styled-components";
 
-import './App.css';
+const VideoPlayer = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100vh;
+  background-color: #fff;
+`
+
+const VideoContainer = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 1200px;
+  height: 640px;
+  max-width: 100%;
+  max-height: 100%;
+  background-color: #000;
+
+  video {
+    width: 100%;
+    height: 100%;
+    max-width: 100%;
+    max-height: 100%;
+  }
+`
 
 function App() {
   return (
-    <div className="Video-player">
-      Video player
-    </div>
+    <VideoPlayer>
+      <VideoContainer>
+        <video controls>
+          <source src="/example.mp4" type="video/mp4" />
+          Your browser does not support HTML video.
+        </video>
+      </VideoContainer>
+    </VideoPlayer>
   );
 }
 
