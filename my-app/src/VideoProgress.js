105eff702ce7776a20b7401f3de56132ad6dcbcc
Ting Wei Zhuang <ji394sfx87@gmail.com>
2021-11-06 16:27:42 +0800

    feat: 增加進度條緩衝時間軸顯示
    VideoProgress.js 增加 ProgressBuffered 區塊，並使用 Canvas 顯示緩衝時間軸

--- my-app/src/VideoProgress.js
+++ my-app/src/VideoProgress.js
@@ -15,6 +15,15 @@ const ProgressBar = styled.div`
     transform-origin: left;
 `
 
+const ProgressBuffered = styled.canvas`
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: block;
+    width: 100%;
+    height: 100%;
+`
+
 const ProgressDotBall = styled.div`
     position: relative;
     display: block;
@@ -77,9 +86,12 @@ const useVideoProgress = ({
     videoElem = null,
     loadStart = false,
     ProgressFrameElem = null,
+    ProgressBufferedElem = null,
     ProgressBarElem = null,
     ProgressDotElem = null
 }) => {
+    const [bufferedContext, setBufferedContext] = useState(null);
+
     const getProgressScale = useCallback(({
         time = 0
     }) => {
@@ -90,16 +102,40 @@ const useVideoProgress = ({
         return 0;
     }, [videoElem]);
 
-    const timeupdate = useCallback((e) => {
-        if(videoElem && ProgressBarElem && ProgressDotElem) {
-            const progressScale = getProgressScale({
-                time: videoElem.currentTime
-            })
-            const progressPosX = ProgressFrameElem.offsetWidth * progressScale
-            ProgressBarElem.style.transform = `scaleX(${progressScale})`
-            ProgressDotElem.style.transform = `translateX(${progressPosX}px)`
+    useEffect(() => {
+        if(ProgressBufferedElem) {
+            setBufferedContext(ProgressBufferedElem.getContext('2d'));
         }
-    }, [videoElem, ProgressFrameElem, ProgressBarElem, ProgressDotElem, getProgressScale]);
+    }, [ProgressBufferedElem, bufferedContext]);
+
+    const timeupdate = useCallback((e) => {
+        if(videoElem) {
+            if(ProgressBarElem && ProgressDotElem) {
+                const progressScale = getProgressScale({
+                    time: videoElem.currentTime
+                })
+                const progressPosX = ProgressFrameElem.offsetWidth * progressScale
+                ProgressBarElem.style.transform = `scaleX(${progressScale})`
+                ProgressDotElem.style.transform = `translateX(${progressPosX}px)`
+            }
+
+            if(bufferedContext && videoElem.buffered.length) {
+                const scale = ProgressBufferedElem.width / videoElem.duration;
+                bufferedContext.clearRect(0, 0, ProgressBufferedElem.width, ProgressBufferedElem.height);
+                bufferedContext.strokeWidth = 0;
+
+                for (let i = 0; i < videoElem.buffered.length; i++) {
+                    var startX = videoElem.buffered.start(i) * scale;
+                    var endX = videoElem.buffered.end(i) * scale;
+                    var width = endX - startX;
+
+                    bufferedContext.fillStyle = 'rgba(242,111,33,0.3)';
+                    bufferedContext.fillRect(startX, 0, width, ProgressBufferedElem.height);
+                    bufferedContext.rect(startX, 0, width, ProgressBufferedElem.height);
+                }
+            }
+        }
+    }, [videoElem, ProgressFrameElem, ProgressBarElem, ProgressDotElem, getProgressScale, ProgressBufferedElem, bufferedContext]);
 
     useEffect((e) => {
         if(loadStart) {
@@ -189,6 +225,7 @@ const VideoProgress = ({
     loadStart = false
 }) => {
     const ProgressFrameRef = useRef();
+    const ProgressBufferedRef = useRef();
     const ProgressBarRef = useRef();
     const ProgressDotRef = useRef();
 
@@ -197,6 +234,7 @@ const VideoProgress = ({
         videoElem: videoRef.current,
         loadStart: loadStart,
         ProgressFrameElem: ProgressFrameRef.current,
+        ProgressBufferedElem: ProgressBufferedRef.current,
         ProgressBarElem: ProgressBarRef.current,
         ProgressDotElem: ProgressDotRef.current
     });
@@ -221,6 +259,9 @@ const VideoProgress = ({
             <ProgressFrame
                 ref={ProgressFrameRef}
             >
+                <ProgressBuffered
+                    ref={ProgressBufferedRef}
+                ></ProgressBuffered>
                 <ProgressBar
                     ref={ProgressBarRef}
                 ></ProgressBar>
